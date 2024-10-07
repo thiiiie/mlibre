75170c5f8f8f5d0a8bbb31cf3c82086993684187
thix <thierrysales2001@gmail.com>
2024-10-07 00:36:43 -0300

    novo

---
+++ public/rastreio/js/scripts.js
@@ -0,0 +1,33 @@
+function showAd2() {
+    document.getElementById('p1').style.display = 'none';
+    document.getElementById('p2').style.display = 'block';
+
+    document.getElementById('progress1').style.display = 'none';
+    document.getElementById('progress2').style.display = 'block';
+}
+
+function showAd3() {
+    document.getElementById('p2').style.display = 'none';
+    document.getElementById('p3').style.display = 'block';
+
+    document.getElementById('progress2').style.display = 'none';
+    document.getElementById('progress3').style.display = 'block';
+}
+
+
+function showAd4() {
+    document.getElementById('p3').style.display = 'none';
+    document.getElementById('p4').style.display = 'block';
+
+    document.getElementById('progress3').style.display = 'none';
+    document.getElementById('progress4').style.display = 'block';
+}
+
+
+function showAd5() {
+    document.getElementById('p4').style.display = 'none';
+    document.getElementById('p5').style.display = 'block';
+
+    document.getElementById('progress4').style.display = 'none';
+    document.getElementById('progress5').style.display = 'block';
+}
