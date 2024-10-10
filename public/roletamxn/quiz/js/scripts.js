b2d4d433ea6a95a573da4c5cd84feb547b2ca761
thix <thierrysales2001@gmail.com>
2024-10-09 23:58:22 -0300

    novo

---
+++ public/roletamxn/quiz/js/scripts.js
@@ -0,0 +1,75 @@
+function showAd2() {
+    document.getElementById('p1').style.display = 'none';
+    document.getElementById('p2').style.display = 'block';
+
+    document.getElementById('progress1').style.display = 'none';
+    document.getElementById('progress2').style.display = 'block';
+}
+
+function showAd3() {
+    document.getElementById('p2').style.display = 'none';
+    document.getElementById('p3').style.display = 'block';
+
+    document.getElementById('progress2').style.display = 'none';
+    document.getElementById('progress3').style.display = 'block';
+}
+
+
+function showAd4() {
+    document.getElementById('p3').style.display = 'none';
+    document.getElementById('p4').style.display = 'block';
+
+    document.getElementById('progress3').style.display = 'none';
+    document.getElementById('progress4').style.display = 'block';
+}
+
+
+function showAd5() {
+    document.getElementById('p4').style.display = 'none';
+    document.getElementById('p5').style.display = 'block';
+
+    document.getElementById('progress4').style.display = 'none';
+    document.getElementById('progress5').style.display = 'block';
+}
+
+
+let currentQuestion = 1;
+const totalQuestions = 5;
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateProgress();
+});
+
+function nextQuestion() {
+    if (currentQuestion < totalQuestions) {
+        document.getElementById(`question${currentQuestion}`).style.display = 'none';
+        currentQuestion++;
+        document.getElementById(`question${currentQuestion}`).style.display = 'block';
+        updateProgress();
+
+        // Verificar e atualizar o texto do botão para "Enviar" na última pergunta
+        if (currentQuestion === totalQuestions) {
+            document.getElementById('next-button').textContent = 'Enviar';
+        }
+
+        // Executar função específica para cada botão
+        if (currentQuestion === 2) {
+            showAd2();
+        } else if (currentQuestion === 3) {
+            showAd3();
+        } else if (currentQuestion === 4) {
+            showAd4();
+        } else if (currentQuestion === 5) {
+            showAd5();
+        }
+    } else {
+        // Redirecionar para outra página HTML ao final do quiz
+        window.location.href = 'https://www.ml-promociones.online/roletac/girar/' + window.location.search;
+    }
+}
+
+function updateProgress() {
+    const progressPercentage = (currentQuestion / totalQuestions) * 100;
+    document.getElementById('progress').style.width = `${progressPercentage}%`;
+    document.getElementById('progress-text').textContent = `${Math.round(progressPercentage)} %`;
+}
